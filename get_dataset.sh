88a9359281cc6c593fce62a0a449969fac37a8f2
biancaziliotto <bianca.ziliotto@studenti.unipi.it>
2023-12-30 16:22:31 +0100

    modified:   get_dataset.sh

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,4 +1,4 @@
-gdown --fuzzy https://drive.google.com/file/d/1nQtRhCxBY5DND3pYJAqCLVXdsAzAQl3U/view?usp=sharing
+gdown --fuzzy https://drive.google.com/file/d/1GTtcZmSYzaZ1vpqTjsZV3xiq_YlfrFCU/view?usp=sharing
 unzip -o data.zip
 rm -rf __MACOSX
 rm data.zip
