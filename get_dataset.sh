32bb930fb8f373d4b7076276ee1565ea0f715693
biancaziliotto <bianca.ziliotto@studenti.unipi.it>
2024-01-05 19:54:21 +0100

    modified:   1_DU&P.ipynb 	modified:   get_dataset.sh

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,4 +1,4 @@
-gdown --fuzzy https://drive.google.com/file/d/1GTtcZmSYzaZ1vpqTjsZV3xiq_YlfrFCU/view?usp=sharing
+gdown --fuzzy https://drive.google.com/file/d/10QQEkfazKv6PW_Q1z8uFjHbf6IQYh1M_/view?usp=sharing
 unzip -o data.zip
 rm -rf __MACOSX
 rm data.zip
